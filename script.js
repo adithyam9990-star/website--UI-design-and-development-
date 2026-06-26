894c6e4529054298844c84e83c6c7bb535a460ca
Adithya M R <adithyam9990@gmail.com>
2026-06-26 11:49:50 +0530

    Implement hover effects on card elements
    Add mouseover and mouseout effects for card elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mouseover', () => {
+    card.style.boxShadow = '0 10px 30px rgba(56,189,248,0.3)';
+  });
 
+  card.addEventListener('mouseout', () => {
+    card.style.boxShadow = 'none';
+  });
+});
